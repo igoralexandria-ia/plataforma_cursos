4e1c7efbf660232909b51d98dc00099a4dc1a869
Igor Alexandria <igoralexandriafullstack@gmail.com>
2024-05-25 17:10:45 -0300

    first commit

---
+++ usuarios/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def cadastro(request):
+    return render(request, 'cadastro.html')
+
+def login(request):
+    return render(request, 'login.html')
